700ad1a539173ae265f084f145f7b5c15f1dfa02
Salbi Faza <salbifaza13@gmail.com>
2025-08-20 10:04:34 +0000

    also edit the footer cause it's different with my reference

--- src/components/site-footer.tsx
+++ src/components/site-footer.tsx
@@ -2,8 +2,8 @@ import { portfolioData } from "@/data/portfolio-data";
 
 export function SiteFooter() {
   return (
-    <footer className="bg-muted dark:bg-black py-6">
-      <div className="container mx-auto px-6 md:px-12 text-center text-muted-foreground">
+    <footer className="bg-gray-900 dark:bg-black py-6">
+      <div className="container mx-auto px-6 md:px-12 text-center text-gray-400">
         <p>&copy; {new Date().getFullYear()} {portfolioData.name}. All Rights Reserved.</p>
       </div>
     </footer>
